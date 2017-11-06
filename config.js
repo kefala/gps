d7b77ca541a98a41bd3767a2cde6e14e1b1c2c8a
Martin Lobo <martin.g.lobo@gmail.com>
2017-11-06 19:02:00 -0300

    Pwa

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || 9000,
-  files: ['./src/**/*.{html,htm,css,js}'],
+  files: ['./src/**/*.{html,htm,css,js, json}'],
   server:{
       baseDir: "./src"
   }
