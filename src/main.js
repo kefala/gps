a519395337e11fecf45bcb49d04415e8d3ea9da1
Martin Lobo <martin.g.lobo@gmail.com>
2017-11-06 18:27:22 -0300

    Init

---
+++ src/main.js
@@ -0,0 +1,20 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const x = document.getElementById('demo');
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+    function showPosition(position) {
+        x.innerHTML = "Latitude: " + position.coords.latitude + 
+        "<br>Longitude: " + position.coords.longitude +
+        "<br>Accuracy: " + position.coords.accuracy; 
+    }
+
+    getLocation();
+});
+
